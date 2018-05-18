c4968a05ea4908b782e6adf1fd49661bde966a43
hengzhaoqing <hengzhaoqing@163.com>
2018-05-18 09:36:33 +0800

    代码重构,删除无用，测试代码

--- hzqing-blog/blog-admin/src/main/java/hzqing/com/blogadmin/service/sys/ILogService.java
+++
@@ -1,7 +0,0 @@
-package hzqing.com.blogadmin.service.sys;
-
-import hzqing.com.blogadmin.base.service.IBaseService;
-import hzqing.com.blogadmin.entity.sys.Log;
-
-public interface ILogService extends IBaseService<Log> {
-}
